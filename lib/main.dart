523a2d55a9573c5450b8ca7adb58a4615fbbea9c
hrithik32 <sahahrithik9@gmail.com>
2025-01-28 19:00:31 +0600

    day3 complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:learn_flutter/home_page.dart';
+import 'package:learn_flutter/pages/home_page.dart';
+import 'package:learn_flutter/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primaryColorLight: const Color.fromARGB(255, 190, 90, 223),
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,17 +2,16 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   final name = "Hrithik";
-  final age = 22;
+  final age = 25;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Hrithik"),
-        backgroundColor: Colors.blue,
+        title: Text("First App"),
+        // backgroundColor: Colors.blue,
       ),
       body: Center(
-        child: Container(
-            child: Text("Hello my name is $name and I'm $age years old")),
+        child: Text("Hello my name is $name and I'm $age years old"),
       ),
       drawer: Drawer(),
     );
