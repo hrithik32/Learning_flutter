1310317415ac2ff65a207d838e6d885751b0d070
hrithik32 <sahahrithik9@gmail.com>
2025-01-28 17:07:05 +0600

    day1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Material(
-        child: Text("hello flutter"),
+        child: Center(child: Text("hello flutter")),
       ),
     );
   }
